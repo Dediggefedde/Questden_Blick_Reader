377feaba1afc63c803036a1755f2b552b74d7396
JulianB <julian.bergmann@physik.uni-giessen.de>
2020-08-18 00:55:54 +0200

    Used codacy hook commit test

--- app/src/main/java/de/dediggefedde/questden_blick_reader/MainActivity.kt
+++ app/src/main/java/de/dediggefedde/questden_blick_reader/MainActivity.kt
@@ -866,7 +866,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     private fun displayThreadList() {
-        listAdapt.items = displayDataList.take(10)//.takeLast(10) //.joinToString("\n")
+        listAdapt.items = displayDataList.take(10)//.takeLast(10)
         listAdapt.notifyDataSetChanged()
         ingredients_list.scrollToPosition(0)
     }
